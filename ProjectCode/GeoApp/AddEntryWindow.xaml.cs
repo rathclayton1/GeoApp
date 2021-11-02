7da40a2a9b16904492fe5e7c376be6d0971670b9
Clayton Rath <rathc02@uwosh.edu>
2021-11-02 11:32:11 -0500

    TS-3 Add textboxes, create methods in CS

--- ProjectCode/GeoApp/AddEntryWindow.xaml.cs
+++ ProjectCode/GeoApp/AddEntryWindow.xaml.cs
@@ -17,11 +17,44 @@ namespace GeoApp
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class AddEntryWindow : Window
     {
-        public Window1()
+        IController controller;
+        public AddEntryWindow()
         {
             InitializeComponent();
+            this.controller = new Controller();
         }
+
+        public void Submit()
+        {
+            List<string> sampleInfo = new List<string>();
+            sampleInfo.Add(SampleID.Text);
+            sampleInfo.Add(Name.Text);
+            sampleInfo.Add(SampleType.Text);
+            sampleInfo.Add(GeologicAge.Text);
+            sampleInfo.Add(City.Text);
+            sampleInfo.Add(State.Text);
+            sampleInfo.Add(Country.Text);
+            sampleInfo.Add(Latitude.Text);
+            sampleInfo.Add(Longitude.Text);
+            controller.CreateNewSample(sampleInfo);
+            System.Environment.Exit(0);
+        }
+
+        public void AddImage()
+        {
+
+        }
+        public void Cancel()
+        {
+
+        }
+
+        public List<string> CleanInput(List<string>)
+        {
+            return new List<string>();
+        }
+
     }
 }
--- ProjectCode/GeoApp/MainWindow.xaml.cs
+++ ProjectCode/GeoApp/MainWindow.xaml.cs
@@ -24,5 +24,6 @@ namespace GeoApp
         {
             InitializeComponent();
         }
+
     }
 }
