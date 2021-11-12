439f975b11da63d2d5a0a060f822e2ffd7afe99e
rathc02 <91558328+rathc02@users.noreply.github.com>
2021-11-11 19:19:19 -0600

    TS-4 Implement EditSample Window and initial logic
    -Properties in edit window are filled with existing data
    -MainWindow needs to add edit button, and pass controller and sampleId of the sample to edit

---
+++ ProjectCode/GeoApp/SuccessfulEditWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GeoApp
+{
+    /// <summary>
+    /// Interaction logic for SuccessfulEditWindow.xaml
+    /// </summary>
+    public partial class SuccessfulEditWindow : Window
+    {
+        public SuccessfulEditWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Close the window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Exit(object sender, RoutedEventArgs e) => this.Close();
+    }
+}
