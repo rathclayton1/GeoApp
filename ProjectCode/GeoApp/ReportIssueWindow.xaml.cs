a76cbd98e372364eb3cf870cf2925f172bd827bf
rathc02 <91558328+rathc02@users.noreply.github.com>
2021-11-29 18:56:57 -0600

    Create report issue window, send issue info to controller

---
+++ ProjectCode/GeoApp/ReportIssueWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GeoApp
+{
+    /// <summary>
+    /// Interaction logic for ReportIssueWindow.xaml
+    /// </summary>
+    public partial class ReportIssueWindow : Window
+    {
+        private IController _controller;
+        public ReportIssueWindow(Controller controller)
+        {
+            InitializeComponent();
+            _controller = controller;
+        }
+
+        private void Submit(object sender, RoutedEventArgs e)
+        {
+            List<String> issue = new();
+            issue.Add(Type.SelectedItem.ToString() == "Misinformation" ? "0" : "1");
+            issue.Add(Description.Text);
+            _controller.CreateIssue(issue);
+            this.Close();
+        }
+
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
