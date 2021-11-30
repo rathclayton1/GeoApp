d176864ae25daaf2d960d2ba78f57e19fac7128d
Demetrios Green <greend40@uwosh.edu>
2021-11-29 18:44:01 -0600

    Initial SampleViewWindow created. Further integration into MainWindow pending.

---
+++ ProjectCode/GeoApp/SampleViewWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace GeoApp
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class SampleViewWindow : Window
+    {
+        public Sample _sample;
+        public SampleViewWindow(Sample sample)
+        {
+            InitializeComponent();
+
+            _sample = sample;
+            Resources.Add("sampleId", _sample.SampleId);
+            Resources.Add("sampleName", _sample.Name);
+            Resources.Add("sampleType", _sample.SampleType);
+            Resources.Add("sampleLocationDescription", _sample.LocationDescription);
+            Resources.Add("sampleGeologicAge", _sample.GeologicAge);
+            Resources.Add("sampleCity", _sample.City);
+            Resources.Add("sampleState", _sample.State);
+            Resources.Add("sampleCountry", _sample.Country);
+            Resources.Add("sampleLatitude", _sample.Latitude);
+            Resources.Add("sampleLongitude", _sample.Longitude);
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
